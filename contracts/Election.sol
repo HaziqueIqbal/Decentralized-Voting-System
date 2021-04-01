d6b35ee51fdc2e4461ecf0722cef3858df3fa369
HaziqueIqbal <haziqiqbal2101@gmail.com>
2021-04-01 14:05:22 -0700

    Version 1.0

---
+++ contracts/Election.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+contract Election {
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    mapping(address => bool) public voters;
+
+    mapping(uint => Candidate) public candidates;
+
+    uint public candidatesCount;
+
+
+    constructor() public {
+        addCandidate("Pakistan Tehreek e Insaf");
+        addCandidate("Pakistan Muslim League-Nawaz");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    
+
+    function vote(uint _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+
+    }
+}
